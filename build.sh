4057542a219c16126ed7b1fd641303e7160b9ff5
Mig Yang <temashking@foxmail.com>
2022-10-07 15:02:26 +0800

    add grpc support

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 
 rm -rf /root/Download/openwrt/package/libs/c-ares
 rm -rf /root/Download/openwrt/package/libs/protobuf
+rm -rf /root/Download/openwrt/package/libs/grpc
 
 cp -r c-ares /root/Download/openwrt/package/libs
 cp -r protobuf /root/Download/openwrt/package/libs
+cp -r grpc /root/Download/openwrt/package/libs
+#cp -r boringssl /root/Download/openwrt/package/libs
